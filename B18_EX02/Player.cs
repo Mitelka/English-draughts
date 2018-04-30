7d7a5c9a1117f75c9f720cf3a3439a9da511f881
Diamant <ndiamant@paypal.com>
2018-04-30 19:49:58 +0300

    change names

--- B18_EX02/Player.cs
+++ B18_EX02/Player.cs
@@ -2,31 +2,30 @@
 {
     internal class Player
     {
-        private ePlayerType playerType;
-        private eSign sign;
-        private int score;
-        private string playerName;
+        private ePlayerType m_PlayerType;
+        private eSign m_Sign;
+        private int m_Score;
+        private string m_PlayerName;
 
         internal class PlayerMovelist
         {
             public Cell originalCell { get; set; }
             public Cell desiredCell { get; set; }
-
         }
 
         public Player(ePlayerType i_PlayerType, eSign i_Sign, string i_playerName)
         {
-            PlayerType = i_PlayerType;
-            Sign = i_Sign;
-            playerName = PlayerName = i_playerName;
+            m_PlayerType = i_PlayerType;
+            m_Sign = i_Sign;
+            m_PlayerName = i_playerName;
         }
 
-        public ePlayerType PlayerType { get; set; }
+        public ePlayerType PlayerType { get => m_PlayerType; set => m_PlayerType = value; }
 
-        public eSign Sign { get; set; }
+        public eSign Sign { get => m_Sign; set => m_Sign = value; }
 
-        public int Score { get; set; }
+        public int Score { get => m_Score; set => m_Score = value; }
 
-        public string PlayerName { get; set; }
+        public string PlayerName { get => m_PlayerName; set => m_PlayerName = value; }
     }
 }
